fa56746ec0558b5baffacba199e251e210c8356a
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-10-10 23:40:42 +0530

    toast and routes added

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,20 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Admin from './pages/Admin';
+import UserDashboard from './pages/UserDashboard';
+import { Toaster } from 'react-hot-toast';
 
 
 function App() {
   return (
     <>
+      <Toaster position='top-center' reverseOrder={false}/>
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Login/>} />
           <Route exact path="/register" element={<Register/>} />
+          <Route exact path="/dashboard" element={<UserDashboard/>} />
+          <Route exact path="/admin" element={<Admin/>} />
         </Routes>
       </BrowserRouter>
      
