1e2bfacc8a05e5d0eeecd4b2f96c7ff4562fb82a
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-10-12 19:49:30 +0530

    sidebar added

--- client/src/components/Dashboard/Navbar.jsx
+++ client/src/components/Dashboard/Navbar.jsx
@@ -1,34 +1,51 @@
 import React, { useState, useEffect } from "react";
 import avatar from "../../assets/avatar.png";
 import "./Navbar.css";
-import { FaPlus } from "react-icons/fa";
+import { FaPlus, FaUser } from "react-icons/fa";
 import { postSoil } from "../../apis/soil";
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 import { ImCancelCircle } from "react-icons/im";
+import { HiOutlineLogout } from "react-icons/hi";
+import { CgProfile } from "react-icons/cg";
+import {IoIosArrowDown } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
   const [characteristics, setCharacteristics] = useState([]);
-  const [suitable_crops, setSuitable_crops] = useState([]); 
-  const [characteristicInput, setCharacteristicInput] = useState(""); 
-  const [cropInput, setCropInput] = useState(""); 
+  const [suitable_crops, setSuitable_crops] = useState([]);
+  const [characteristicInput, setCharacteristicInput] = useState("");
+  const [cropInput, setCropInput] = useState("");
 
-  const [name, setName] = useState('');
-  const [color, setColor] = useState('');
-  const [distributor, setDistributor] = useState('');
+  const [name, setName] = useState("");
+  const [color, setColor] = useState("");
+  const [distributor, setDistributor] = useState("");
   const [token, setToken] = useState(null);
+  const [avatarModal, setAvatarModal] = useState(false);
+  const [closing, setClosing] = useState(false); // New state for handling closing animation
+
+  const handleAvatarModalToggle = () => {
+    if (avatarModal) {
+      setClosing(true);
+      setTimeout(() => {
+        setAvatarModal(false);
+        setClosing(false);
+      }, 500); 
+    } else {
+      setAvatarModal(true);
+    }
+  };
 
   useEffect(() => {
-    const tok = localStorage.getItem('token');
+    const tok = localStorage.getItem("token");
     setToken(tok);
-  }, [])
-  
+  }, []);
 
   // Add to Characteristics array
   const handleAddCharacteristic = () => {
     if (characteristicInput.trim() !== "") {
       setCharacteristics([...characteristics, characteristicInput]);
-      setCharacteristicInput(""); 
+      setCharacteristicInput("");
     }
   };
 
@@ -36,41 +53,48 @@ function Navbar() {
   const handleAddCrop = () => {
     if (cropInput.trim() !== "") {
       setSuitable_crops([...suitable_crops, cropInput]);
-      setCropInput(""); 
+      setCropInput("");
     }
   };
 
   const Addsoil = () => {
-    if (!open){
+    if (!open) {
       setOpen(true);
-    }else{
+    } else {
       setOpen(false);
-      setName('');
-      setColor('');
+      setName("");
+      setColor("");
       setCharacteristics([]);
-      setDistributor('');
+      setDistributor("");
       setSuitable_crops([]);
     }
   };
 
-  const newsoil = async(e) => {
+  const newsoil = async (e) => {
     e.preventDefault();
-    const res = await postSoil(name, color, characteristics, suitable_crops, distributor, token)
+    const res = await postSoil(
+      name,
+      color,
+      characteristics,
+      suitable_crops,
+      distributor,
+      token
+    );
     console.log(res);
-    if(res.status === 201){
+    if (res.status === 201) {
       console.log("Soil added successfully");
-      toast.success('Soil added successfully');
-      setName('');
-      setColor('');
+      toast.success("Soil added successfully");
+      setName("");
+      setColor("");
       setCharacteristics([]);
       setSuitable_crops([]);
-      setDistributor('');
+      setDistributor("");
       setOpen(false);
-    } else{
+    } else {
       console.log("Failed to add soil");
-      toast.error('Failed to add soil')
+      toast.error("Failed to add soil");
     }
-  }
+  };
 
   return (
     <>
@@ -79,11 +103,30 @@ function Navbar() {
           <p>&nbsp;</p>
           <h1>soil farming agent</h1>
           <ul>
-            <p>
-              <img src={avatar} alt="avatar" />
+            <p
+              onClick={(e) => {
+                avatarModal ? setAvatarModal(false) : setAvatarModal(true);
+              }}
+            >
+              <img src={avatar} alt="avatar" /><IoIosArrowDown size={24}/>
             </p>
           </ul>
         </nav>
+        {avatarModal && (
+          <div className={`avatar-modal ${closing ? "slide-out" : ""}`}>
+            <p onClick={handleAvatarModalToggle}>
+              <RxCross2 size={32} />
+            </p>
+            <p>
+              <FaUser size={22} />
+              <span>Update Profile</span>
+            </p>
+            <p>
+              <HiOutlineLogout />
+              <span>Log out</span>
+            </p>
+          </div>
+        )}
         <div className="new_distribuutor">
           <button onClick={Addsoil}>
             <FaPlus />
@@ -94,29 +137,48 @@ function Navbar() {
 
       {open && (
         <div className="form-container">
-          <div style={{display:"flex",float: "right", marginRight:'-0.5rem', cursor:'pointer'}}><ImCancelCircle onClick={Addsoil} size={21} /></div>
+          <div
+            style={{
+              display: "flex",
+              float: "right",
+              marginRight: "-0.5rem",
+              cursor: "pointer",
+            }}
+          >
+            <ImCancelCircle onClick={Addsoil} size={21} />
+          </div>
           <h1>New Distributor</h1>
           <form>
             <label>
-              Soil Name : 
-              <input type="text" onChange={(e)=>setName(e.target.value)} name="soilName" required />
+              Soil Name :
+              <input
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+                name="soilName"
+                required
+              />
             </label>
 
             <label>
-              Soil Color : 
-              <input type="text" onChange={(e)=>setColor(e.target.value)} name="color" required />
+              Soil Color :
+              <input
+                type="text"
+                onChange={(e) => setColor(e.target.value)}
+                name="color"
+                required
+              />
             </label>
 
             <div>
               <label>
-                Characteristic : 
+                Characteristic :
                 <input
                   type="text"
                   value={characteristicInput}
                   onChange={(e) => setCharacteristicInput(e.target.value)}
                 />
                 <button className="charbtn" onClick={handleAddCharacteristic}>
-                Add
+                  Add
                 </button>
               </label>
               <ul>
@@ -128,14 +190,18 @@ function Navbar() {
 
             <div>
               <label>
-               <span> Suitable Crop : </span>
+                <span> Suitable Crop : </span>
                 <input
                   type="text"
                   value={cropInput}
                   onChange={(e) => setCropInput(e.target.value)}
                 />
-                <button className="charbtn" type="button" onClick={handleAddCrop}>
-                Add
+                <button
+                  className="charbtn"
+                  type="button"
+                  onClick={handleAddCrop}
+                >
+                  Add
                 </button>
               </label>
               <ul>
@@ -145,11 +211,19 @@ function Navbar() {
               </ul>
             </div>
             <label>
-              Distributor : 
-              <input type="text" onChange={(e)=>setDistributor(e.target.value)} name="distributor" required />
+              Distributor :
+              <input
+                type="text"
+                onChange={(e) => setDistributor(e.target.value)}
+                name="distributor"
+                required
+              />
             </label>
 
-            <button className="addsoil" onClick={newsoil}><FaPlus />&nbsp; Add Distributor</button>
+            <button className="addsoil" onClick={newsoil}>
+              <FaPlus />
+              &nbsp; Add Distributor
+            </button>
           </form>
         </div>
       )}
